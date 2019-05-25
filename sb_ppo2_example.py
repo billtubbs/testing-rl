5c9d19ccfbb52d5f5e36a63f6134c335fc785159
billtubbs <bill.tubbs@me.com>
2019-05-25 00:31:27 -0700

    Big fixes

--- sb_ppo2_example.py
+++ sb_ppo2_example.py
@@ -1,44 +1,60 @@
 import os
+import argparse
 import numpy as np
 import gym
 import gym_CartPole_BT
 
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
-# I had a problem with my Mac OS X environment which required
-# this fix (see here: https://github.com/dmlc/xgboost/issues/1715)
-# Delete if you don't have this problem:
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+# Parse any arguments provided at the command-line
+parser = argparse.ArgumentParser(description='Test this gym environment.')
+parser.add_argument('-e', '--env', type=str, default='CartPole-BT-dL-v0',
+                    help="gym environment")
+parser.add_argument('-s', "--show", help="display output",
+                    action="store_true")
+parser.add_argument('-r', "--render", help="render animation",
+                    action="store_true")
+args = parser.parse_args()
 
-env_name = 'CartPole-BT-dL-v0'
-env = gym.make(env_name)
+# Create and initialize environment
+if args.show:
+    print(f"\nInitializing environment '{args.env}'...")
+env = gym.make(args.env)
 env = DummyVecEnv([lambda: env])
 
-filename = f"ppo2_{env_name}"
+filename = f"ppo2_{args.env}.pkl"
 if os.path.isfile(filename):
     model = PPO2.load(filename)
     model.set_env(env)
-    print(f"Existing model loaded from file '{filename}'")
+    if args.show:
+        print(f"Existing model loaded from file '{filename}'")
 else:
     model = PPO2(MlpPolicy, env, verbose=1)
 
 # Train model
 model.learn(total_timesteps=50000, log_interval=10)
 model.save(filename)
-print(f"Model saved to file '{filename}'")
+if args.show:
+    print(f"Model saved to file '{filename}'")
 
-# Display animated runs with trained model
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        s = input("Press enter to run again, 'q' to quit: ")
-        if s.lower() == 'q':
-            break
-        obs = env.reset()
+if args.show:
+    # Display animated runs with trained model
+    obs = env.reset()
+    cum_reward = 0.0
+    while True:
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        cum_reward += reward
+        if args.render:
+            env.render()
+        if done:
+            print(f"Reward: {round(cum_reward, 2)}")
+            s = input("Press enter to run again, 'q' to quit: ")
+            if s.lower() == 'q':
+                break
+            obs = env.reset()
+            cum_reward = 0.0
 
 env.close()
--- sb_sac_example.py
+++ sb_sac_example.py
@@ -1,44 +1,60 @@
 import os
-import gym
+import argparse
 import numpy as np
+import gym
 import gym_CartPole_BT
 
 from stable_baselines.sac.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import SAC
 
-# I had a problem with my Mac OS X environment which required
-# this fix (see here: https://github.com/dmlc/xgboost/issues/1715)
-# Delete if you don't have this problem:
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+# Parse any arguments provided at the command-line
+parser = argparse.ArgumentParser(description='Test this gym environment.')
+parser.add_argument('-e', '--env', type=str, default='CartPole-BT-dL-v0',
+                    help="gym environment")
+parser.add_argument('-s', "--show", help="display output",
+                    action="store_true")
+parser.add_argument('-r', "--render", help="render animation",
+                    action="store_true")
+args = parser.parse_args()
 
-env_name = 'CartPole-BT-vH-v0'
-env = gym.make(env_name)
+# Create and initialize environment
+if args.show:
+    print(f"\nInitializing environment '{args.env}'...")
+env = gym.make(args.env)
 env = DummyVecEnv([lambda: env])
 
-filename = f"sac_{env_name}.pkl"
+filename = f"sac_{args.env}.pkl"
 if os.path.isfile(filename):
     model = SAC.load(filename)
     model.set_env(env)
-    print(f"Existing model loaded from file '{filename}'")
+    if args.show:
+        print(f"Existing model loaded from file '{filename}'")
 else:
     model = SAC(MlpPolicy, env, verbose=1)
 
 # Train model
-model.learn(total_timesteps=50000, log_interval=10)
+model.learn(total_timesteps=100, log_interval=10)
 model.save(filename)
-print(f"Model saved to file '{filename}'")
+if args.show:
+    print(f"Model saved to file '{filename}'")
 
-# Display animated runs with trained model
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        s = input("Press enter to run again, 'q' to quit: ")
-        if s.lower() == 'q':
-            break
-        obs = env.reset()
+if args.show:
+    # Display animated runs with trained model
+    obs = env.reset()
+    cum_reward = 0.0
+    while True:
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        cum_reward += reward
+        if args.render:
+            env.render()
+        if done:
+            print(f"Reward: {round(cum_reward, 2)}")
+            s = input("Press enter to run again, 'q' to quit: ")
+            if s.lower() == 'q':
+                break
+            obs = env.reset()
+            cum_reward = 0.0
 
 env.close()
