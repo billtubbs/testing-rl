70ef83cb26efdb8b918af8c3a74a6ee40d7779ce
billtubbs <bill.tubbs@me.com>
2019-05-24 22:36:33 -0700

    Added command-line args

--- test_run_lqr_s.py
+++ test_run_lqr_s.py
@@ -1,35 +1,22 @@
 import gym
 import gym_CartPole_BT
 import numpy as np
-import argparse
-
-# Parse any arguments provided at the command-line
-parser = argparse.ArgumentParser(description='Test this gym environment.')
-parser.add_argument('-e', '--env', type=str, default='CartPole-BT-dL-v0',
-                    help="gym environment")
-parser.add_argument('-s', "--show", help="display output",
-                    action="store_true")
-parser.add_argument('-r', "--render", help="render animation",
-                    action="store_true")
-args = parser.parse_args()
 
 # Create and initialize environment
-if args.show: print(f"\nInitializing environment '{args.env}'...")
-env = gym.make(args.env)
+env = gym.make('CartPole-BT-m2-dL-v0')
 env.reset()
 
 # Control vector (shape (1, ) in this case)
 u = np.zeros(1)
 
 # Open graphics window and draw animation
-if args.render: env.render()
+#env.render()
 
 # We will keep track of the cumulative rewards
 cum_reward = 0.0
 
-if args.show:
-    print(f"{'k':>3s}  {'u':>5s} {'reward':>6s} {'cum_reward':>10s}")
-    print("-"*28)
+print(f"{'k':>3s}  {'u':>5s} {'reward':>6s} {'cum_reward':>10s}")
+print("-"*28)
 
 # Gain matrix (K) for optimal control
 # (Calculated using lqr function with Q=np.eye(4), and R=0.0001)
@@ -50,17 +37,15 @@ while not done:
     observation, reward, done, info = env.step(u)
 
     # Update the animation
-    if args.render: env.render()
+    #env.render()
 
     # Process the reward
     cum_reward += reward
 
     # Print updates
-    if args.show:
-        print(f"{env.time_step:3d}: {u[0]:5.1f} {reward:6.2f} "
-              f"{cum_reward:10.1f}")
+    print(f"{env.time_step:3d}: {u[0]:5.1f} {reward:6.2f} {cum_reward:10.1f}")
 
 input("Press enter to continue...")
 
 # Close animation window
-env.close()
+#env.viewer.close()
--- test_run_random.py
+++ test_run_random.py
@@ -3,6 +3,19 @@ import pandas as pd
 import gym
 import gym_CartPole_BT
 from control_baselines import LQR
+import argparse
+
+# Parse any arguments provided at the command-line
+parser = argparse.ArgumentParser(description='Test this gym environment.')
+parser.add_argument('-e', '--env', type=str, default='CartPole-BT-dL-v0',
+                    help="gym environment")
+parser.add_argument('-s', "--show", help="display output",
+                    action="store_true")
+parser.add_argument('-r', "--render", help="render animation",
+                    action="store_true")
+parser.add_argument('-v', "--verbose", help="increase output verbosity",
+                    action="store_true")
+args = parser.parse_args()
 
 def run_episode(env, model, render=True, show=True):
 
@@ -46,9 +59,9 @@ def run_episode(env, model, render=True, show=True):
 
 
 # Create and initialize environment
-env_name = 'CartPole-BT-dL-vL-v0'
-print(f"\nInitializing environment '{env_name}'...")
-env = gym.make(env_name)
+if args.show: print(f"\nInitializing environment '{args.env}'...")
+env = gym.make(args.env)
+env.reset()
 
 model = LQR(None, env)
 
@@ -72,7 +85,8 @@ except:
     n_iter, top_n = (100, 3)
 
 # Start random search over full search area
-print(f"\nStarting random search for {n_iter} episodes...")
+if args.show:
+    print(f"\nStarting random search for {n_iter} episodes...")
 results = []
 for i in range(n_iter):
     gain = (np.random.random(size=(1, 4)) - 0.5)*search_size
@@ -83,38 +97,41 @@ for i in range(n_iter):
 top_results = pd.DataFrame(results, columns=['cum_reward', 'gain'])
 top_results = top_results.sort_values(by='cum_reward', ascending=False)
 top_results = top_results.reset_index(drop=True).head(top_n)
-print(f"Top {top_n} results after {n_iter} episodes:\n"
-      f"{top_results[['cum_reward']].round(2)}")
+if args.show:
+    print(f"Top {top_n} results after {n_iter} episodes:\n"
+          f"{top_results[['cum_reward']].round(2)}")
 
 best_gain = top_results.loc[0, 'gain']
 std_gain = np.vstack(top_results['gain'].values).std(axis=0)
 
 df = pd.DataFrame({'Best': best_gain.ravel(), 'Std.': std_gain})
-print(f"Best gain values and std. dev:\n{df.round(3)}")
+if args.show:
+    print(f"Best gain values and std. dev:\n{df.round(3)}")
+    print(f"\nStarting targetted search for {n_iter} episodes...")
 
-print(f"\nStarting targetted search for {n_iter} episodes...")
 # Now search within reduced area
 for i in range(n_iter):
     gain = np.random.normal(best_gain, std_gain)
     model = LQR(None, env, gain)
-    cum_reward = run_episode(env, model, render=False, show=False)
+    cum_reward = run_episode(env, model, render=args.render, show=False)
     #print(f"{i}: Cum reward: {cum_reward}")
     results.append((cum_reward, gain))
 
 top_results = pd.DataFrame(results, columns=['cum_reward', 'gain'])
 top_results = top_results.sort_values(by='cum_reward', ascending=False)
 top_results = top_results.reset_index(drop=True).head(top_n)
-print(f"Top {top_n} results after {n_iter} episodes:\n"
-      f"{top_results[['cum_reward']].round(2)}")
+if args.show:
+    print(f"Top {top_n} results after {n_iter} episodes:\n"
+          f"{top_results[['cum_reward']].round(2)}")
+    print(f"\nStarting robustness checks on top {top_n} results...")
 
-print(f"\nStarting robustness checks on top {top_n} results...")
 results = []
 # Do a robustness check on top results:
 for gain in top_results['gain']:
     model = LQR(None, env, gain)
     # Average over 3 runs
     mean_reward = np.mean([
-        run_episode(env, model, render=False, show=False)
+        run_episode(env, model, render=args.render, show=False)
         for _ in range(3)
     ])
     results.append(mean_reward)
@@ -122,21 +139,24 @@ for gain in top_results['gain']:
 best = np.argmax(results)
 best_reward, best_gain = top_results.loc[best]
 
-print(f"\nBest result (#{best}):")
-print(f" Reward: {round(best_reward, 2)}")
-print(f" Gain: {best_gain.round(3)}")
+if args.show:
+    print(f"\nBest result (#{best}):")
+    print(f" Reward: {round(best_reward, 2)}")
+    print(f" Gain: {best_gain.round(3)}")
 
-input("\nPress enter to start simulation...")
+    input("\nPress enter to start simulation...")
 
 model.gain = best_gain
 
 # Run repeated simulations with animation
-while True:
-    cum_reward = run_episode(env, model, render=True, show=False)
-    print(f"Reward: {round(cum_reward, 2)}")
-    s = input("Press enter to run again, 'q' to quit: ")
-    if s.lower() == 'q':
-        break
+if args.show:
+    while True:
+        cum_reward = run_episode(env, model, render=args.render,
+                                 show=args.verbose)
+        print(f"Reward: {round(cum_reward, 2)}")
+        s = input("Press enter to run again, 'q' to quit: ")
+        if s.lower() == 'q':
+            break
 
 # Close animation window
-env.viewer.close()
+env.close()
