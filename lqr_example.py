d52090c664cb3ef3da395a714847e0f9e4a6a583
billtubbs <bill.tubbs@me.com>
2019-05-25 00:00:08 -0700

    Added command-line arguments

--- lqr_example.py
+++ lqr_example.py
@@ -1,22 +1,41 @@
+import argparse
 import gym
 import gym_CartPole_BT
 from control_baselines import LQRCartPend
 
-env_name = 'CartPole-BT-dH-vH-v0'
-env = gym.make(env_name)
+# Parse any arguments provided at the command-line
+parser = argparse.ArgumentParser(description='Test this gym environment.')
+parser.add_argument('-e', '--env', type=str, default='CartPole-BT-dL-v0',
+                    help="gym environment")
+parser.add_argument('-s', "--show", help="display output",
+                    action="store_true")
+parser.add_argument('-r', "--render", help="render animation",
+                    action="store_true")
+args = parser.parse_args()
+
+# Create and initialize environment
+if args.show:
+    print(f"\nInitializing environment '{args.env}'...")
+env = gym.make(args.env)
 
 model = LQRCartPend(None, env, verbose=1)
 
-# Display animated runs with model
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, reward, done, info = env.step(action)
-    env.render()
-    if done:
-        s = input("Press enter to run again, 'q' to quit: ")
-        if s.lower() == 'q':
-            break
-        obs = env.reset()
+if args.show:
+    # Display animated runs with model
+    obs = env.reset()
+    cum_reward = 0.0
+    while True:
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        cum_reward += reward
+        if args.render:
+            env.render()
+        if done:
+            print(f"Reward: {round(cum_reward, 2)}")
+            s = input("Press enter to run again, 'q' to quit: ")
+            if s.lower() == 'q':
+                break
+            obs = env.reset()
+            cum_reward = 0.0
 
 env.close()
