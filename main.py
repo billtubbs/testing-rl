c62e3f42e5154817977fbee16eda07891b77faf2
billtubbs <bill.tubbs@me.com>
2019-05-25 14:32:18 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,120 @@
+import os
+import time
+import platform
+import random
+import subprocess
+import numpy as np
+import yaml
+import gym
+
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+from stable_baselines.ddpg.policies import MlpPolicy
+from stable_baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines import DDPG
+from stable_baselines.ddpg.noise import AdaptiveParamNoiseSpec
+
+from parsers import parser
+
+# Parse input arguments with parsers module
+args = parser.parse_args()
+
+# Add additional parameters
+args.start_time = time.strftime('%Y-%m-%d %H:%M:%S')
+args.hostname = platform.node()
+
+# Unique identifier for this simulation
+args.id = time.strftime('%y%m%d%H%M%S') + args.hostname.replace('.', '_')
+
+# For PyTorch use the following:
+# args.has_cuda = torch.cuda.is_available()
+# if args.has_cuda:
+#     args.num_gpus = torch.cuda.device_count()
+# else:
+#     args.num_gpus = 0
+# torch.manual_seed(args.seed)
+
+# For Tensorflow use the following:
+args.has_cuda = tf.test.is_gpu_available()
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
+if args.has_cuda:
+    args.num_gpus = len(get_available_gpus())
+else:
+    args.num_gpus = 0
+tf.random.set_random_seed(args.seed)
+
+# Just in case these are used somewhere:
+random.seed(args.seed)
+np.random.seed(args.seed)
+
+# Record git hash, so we know what the code looked like when this ran
+args.git_hash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip().decode()
+
+# Create log dir
+if args.log_dir is not None:
+    os.makedirs(args.log_dir, exist_ok=True)
+
+    if args.overwrite is False and len(os.listdir(args.log_dir)) > 1:
+        raise FileExistsError("There are existing files in '" + args.log_dir +
+                              "'. Use --overwrite argument to over-write them.")
+
+def callback(_locals, _globals):
+    """
+    Callback called at each step (for DQN an others) or after n steps
+    (see ACER or PPO2).
+    :param _locals: (dict)
+    :param _globals: (dict)
+    """
+    global n_steps, best_mean_reward
+    # Print stats every 1000 calls
+    if (n_steps + 1) % 1000 == 0:
+        # Evaluate policy performance
+        x, y = ts2xy(load_results(args.log_dir), 'timesteps')
+        if len(x) > 0:
+            mean_reward = np.mean(y[-100:])
+            print(x[-1], 'timesteps')
+            print(f"Best mean reward: {best_mean_reward:.2f} - "
+                  f"Last mean reward: {mean_reward:.2f}")
+
+            # New best model, you could save the agent here
+            if mean_reward > best_mean_reward:
+                best_mean_reward = mean_reward
+                # Example for saving best model
+                print("Saving new best model")
+                _locals['self'].save(args.log_dir + 'best_model.pkl')
+    n_steps += 1
+    return True
+
+
+def main():
+
+    # Save argument values to yaml file
+    args_file_path = os.path.join(args.log_dir, 'args.yaml')
+    with open(args_file_path, 'w') as f:
+        yaml.dump(vars(args), f, default_flow_style=False)
+
+    # Create and wrap the environment
+    env = gym.make(args.env)
+    env = Monitor(env, log_dir, allow_early_resets=True)
+    env = DummyVecEnv([lambda: env])
+
+    # Add some param noise for exploration
+    param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.2,
+                                         desired_action_stddev=0.2)
+    model = DDPG(MlpPolicy, env, param_noise=param_noise,
+                 memory_limit=int(1e6), verbose=0)
+
+    # Train the agent
+    model.learn(total_timesteps=args.n_steps, callback=callback)
+
+    if args.plots:
+        raise NotImplementedError
+        # TODO: utils.make_learning_curve_plots(args.log_dir)
+
+if __name__ == '__main__':
+    main()
---
+++ parsers.py
@@ -0,0 +1,42 @@
+"""This module parses all command line arguments to main.py"""
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser('Testing RL Algorithms in Tensorflow')
+parser.add_argument('--env', type=str, metavar='ENV-ID',
+                    help="Gym environment id. E.g. 'CartPole-v1'")
+parser.add_argument('--model', type=str, metavar='MODEL-ID',
+                    help="RL algorithm name")
+parser.add_argument('--name', type=str, default=None, metavar='LOG-NAME',
+                    help="Meaningful label to describe this run. If None, a "
+                    "name is created with the environment name, model name "
+                    "and timestamp at start of training.")
+parser.add_argument('--comment', type=str, default='', metavar='COMMENT',
+                    help="user-defined text to describe this run or the "
+                    "project or experiment that it relates to (default: '')")
+parser.add_argument('--n_steps', type=int, default=50000, metavar='N',
+                    help="Total number of samples to train on")
+parser.add_argument('--seed', type=int, default=None, metavar='S',
+                    help='Initial seed for training')
+parser.add_argument('--reset-timesteps', action='store_true', default=False,
+                    help="Whether or not to reset the current timestep "
+                    "number (used in logging).")
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help="Number of timesteps before logging (default: 10)")
+parser.add_argument('--log_dir', type=str, default='.',
+                    help="directory for outputting log files. (Default: '.')")
+parser.add_argument('--save-model', action='store_true', default=False,
+                    help="save model after each epoch. If no log directory "
+                         "is provided, saves to the current directory.")
+parser.add_argument('--overwrite', action='store_true', default=False,
+                    help="Without this argument, the script will raise a "
+                         "FileExistsError if you try to write to an "
+                         "existing log-dir output directory.")
+parser.add_argument('--plots', action='store_true', default=False,
+                    help="Automatically generate plots of learning curves.")
+
+group1 = parser.add_argument_group('Model hyperparameters')
+# TODO: Add model hyper-parameters here...
+group1.add_argument('--model-args',type=str, default='{}',
+                    help="A dictionary of extra arguments passed to the "
+                    "model. (default: '{}')")
