e1cd3b04aefedfd6c80949e06a05df0a011255af
billtubbs <bill.tubbs@me.com>
2019-11-01 17:35:14 -0700

    First commit

---
+++ run_brs_experiments.sh
@@ -0,0 +1,5 @@
+python test_run_bsr.py -e 'CartPole-BT-vL-v0' -s 1 -l -r -f -m 100
+python test_run_bsr.py -e 'CartPole-BT-vL-v0' -s 2 -l -r -f -m 100
+python test_run_bsr.py -e 'CartPole-BT-vL-v0' -s 3 -l -r -f -m 100
+python test_run_bsr.py -e 'CartPole-BT-vL-v0' -s 4 -l -r -f -m 100
+python test_run_bsr.py -e 'CartPole-BT-vL-v0' -s 5 -l -r -f -m 100
