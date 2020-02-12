d4ef40aa994f83b3a08255618de2364868860ff0
billtubbs <bill.tubbs@me.com>
2020-02-12 15:00:18 -0800

    env config arg

--- test_tune_cartpole.py
+++ test_tune_cartpole.py
@@ -8,26 +8,44 @@ You can visualize experiment results in ~/ray_results using TensorBoard.
 import numpy as np
 import gym
 from gym_CartPole_BT.envs.cartpole_bt_env import CartPoleBTEnv
-
 import ray
 from ray import tune
+from ray.tune.registry import register_env
 from ray.tune import grid_search
 
+
+def env_creator(env_config):
+    """Return an env instance with given config"""
+    return CartPoleBTEnv(
+        goal_state=env_config['goal_state'],
+        disturbances=env_config['disturbances'],
+        initial_state=env_config['initial_state'],
+        initial_state_variance=env_config['initial_state_variance']
+    )
+
 # Can also register the env creator function explicitly with:
-# register_env("corridor", lambda config: SimpleCorridor(config))
+register_env("CartPole-BT-v0", env_creator)
+#trainer = ppo.PPOTrainer(env="CartPole-BT-v0")
+
+config = {
+    "env": "CartPole-BT-v0",
+    "seed": args.seed,
+    "num_gpus": 0,
+    "num_workers": args.num_workers,
+    "num_cpus_per_worker": 1,
+    "env_config": {
+                    'goal_state': [0.0, 0.0, np.pi, 0.0],
+                    'disturbances': 'low',
+                    'initial_state': 'goal',
+                    'initial_state_variance': None
+                },
+}
 
 tune.run(
     "PPO",
     stop={
         "timesteps_total": 10000,
     },
-    config={
-        "env": CartPoleBTEnv,  # or "env-name" if registered above
-        "vf_share_layers": True,
-        "lr": grid_search([1e-2, 1e-4, 1e-6]),  # try different lrs
-        "num_workers": 47,  # parallelism
-        "env_config": {
-            # TODO: Specify env parameters
-        },
+    config=config,
     },
 )
