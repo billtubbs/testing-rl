bac275cb44cb87a7ebf8d63e2faac8e8260b1fce
billtubbs <bill.tubbs@me.com>
2020-02-12 14:55:42 -0800

    env config args

--- test_run_cartpole.py
+++ test_run_cartpole.py
@@ -1,13 +1,23 @@
-# Based on example here:
-# https://ray.readthedocs.io/en/latest/rllib.html
+"""Example of training an rllib algorithm on a custom gym
+environment and model.
 
+Based on examples here:
+https://ray.readthedocs.io/en/latest/rllib.html
+
+And here:
+https://github.com/ray-project/ray/blob/master/rllib/examples/custom_env.py
+"""
+
+
+import argparse
+import numpy as np
 import ray
 import ray.rllib.agents.ppo as ppo
 from ray.rllib.agents.ppo import PPOTrainer
+from ray.tune.registry import register_env
 from ray import tune
 import gym
 from gym_CartPole_BT.envs.cartpole_bt_env import CartPoleBTEnv
-import argparse
 
 
 # Parse command-line arguments
@@ -16,18 +26,42 @@ parser.add_argument('--n_iters', type=int, default=1000, metavar='N',
                     help="Total number of training iterations")
 parser.add_argument('--seed', type=int, default=None, metavar='S',
                     help='Initial seed for training')
+parser.add_argument('--num_workers', type=int, default=2,
+                    help='Number of parallel workers')
+parser.add_argument('--num_gpus', type=int, default=0,
+                    help='Number of GPUs to use')
+
 args = parser.parse_args()
 
+
+def env_creator(env_config):
+    """Return an env instance with given config"""
+    return CartPoleBTEnv(
+        goal_state=env_config['goal_state'],
+        disturbances=env_config['disturbances'],
+        initial_state=env_config['initial_state'],
+        initial_state_variance=env_config['initial_state_variance']
+    )
+
+register_env("CartPole-BT-v0", env_creator)
+#trainer = ppo.PPOTrainer(env="CartPole-BT-v0")
+
 config = {
-    "env": CartPoleBTEnv,
+    "env": "CartPole-BT-v0",
     "seed": args.seed,
     "num_gpus": 0,
-    "num_workers": 47,
-    "num_cpus_per_worker": 1
+    "num_workers": args.num_workers,
+    "num_cpus_per_worker": 1,
+    "env_config": {
+                    'goal_state': [0.0, 0.0, np.pi, 0.0],
+                    'disturbances': 'low',
+                    'initial_state': 'goal',
+                    'initial_state_variance': None
+                },
 }
 
 stop = {
     "training_iteration": args.n_iters
 }
 
-tune.run(PPOTrainer, config=config, stop=stop)
+tune.run("PPO", config=config, stop=stop)
